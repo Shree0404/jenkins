63102eed9ec600ff1126af1177fb6919a60935e6
Shree0404 <shrinivasuravakonda@gmail.com>
2025-04-22 15:17:58 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    public static void main(String[] args) {
+        
+        String Student_name = "Sandy";
+        String College_name = "kle";
+        int modile_no = 123456789 ;
+        int id = 123;
+        double fee = 100000;
+
+        System.out.println("Student Name:"+ Student_name);
+        System.out.println("Student id:"+ id);        
+        System.out.println("College Name:"+ College_name);
+        System.out.println("Fee:"+ fee);        
+
+        System.out.println("Mobile Number:"+ modile_no);        
+    }
+    
+}
